7d5eebc57a60f22938a75007ce4c7d0bc59a3c67
Tiago Oliveira <contato@otao.dev.br>
2023-08-27 20:33:09 -0300

    ✨ add hooks to redux configs

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import store from './app/store/store';
+import { store } from './app/store';
 
 setupIonicReact();
 
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-import rootReducer from '../../reducers';
+import rootReducer from '../reducers';
 
 export const store = configureStore({
   reducer: rootReducer,
